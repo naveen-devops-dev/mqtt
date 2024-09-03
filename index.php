206b922c50ee4e085732207e415d67452a26f9d3
Naveen V Badami <naveenvbadami@gmail.com>
2024-09-04 00:38:20 +0530

    learning fork

--- index.php
+++ index.php
@@ -8,6 +8,7 @@ $port = 1883;                     // change if necessary
 $username = "";                   // set your username
 $password = "";                   // set your password
 $client_id = "phpMQTT-publisher"; // make sure this is unique for connecting to sever - you could use uniqid()
+learning:wq
 
 
 $mqtt = new phpMQTT($server, $port, $client_id);
